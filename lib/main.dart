043c4050cdcd49b789a3a6db2f2211c5f63f2a73
bilal5717 <145279530+bilal5717@users.noreply.github.com>
2023-09-26 21:38:33 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,70 @@
+// ignore_for_file: library_private_types_in_public_api, non_constant_identifier_names
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+              child: Text('Dice App created by Bilal')
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: const DiceChange(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DiceChange extends StatefulWidget {
+  const DiceChange({super.key});
+
+  @override
+  _DiceChangeState createState() => _DiceChangeState();
+}
+
+class _DiceChangeState extends State<DiceChange> {
+  int num1 = 1;
+  int num2 = 2;
+  void ChangeState() {
+    setState(() {
+      //Random.nextInt(n) returns random integer from 0 to n-1
+      num1 = Random().nextInt(6) + 1;
+      num2 = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child: Text('Dice App' ,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0
-              ),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: TextButton(
+                child: Image.asset('images/dice$num1.png'),
+                onPressed: () {
+                  ChangeState();
+                },
               ),
             ),
           ),
-        body: SafeArea(
-          child: Center(
-            child: Row(
-                children: <Widget>[
-                  Expanded(
-
-                    child: TextButton(
-                      child: Container(
-
-                        padding: const EdgeInsets.all(14),
-                        child: Image.asset('images/dice1.png'),
-                      ),
-                      onPressed: () {
-                        print("button pressed");
-                      },
-                    ),
-                  ),
-                  Expanded(
-
-                    child: TextButton(
-                      child: Container(
-
-                        padding: const EdgeInsets.all(14),
-                        child: Image.asset('images/dice2.png'),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),//Expanded
-                ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: TextButton(
+                child: Image.asset('images/dice$num2.png'),
+                onPressed: () {
+                  ChangeState();
+                },
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
